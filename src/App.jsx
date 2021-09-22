17df1e4eacb6489f38a95210846d2a4fd313cf54
Franco De Alesandro <fdealesandro@tgv.com.ar>
2021-09-21 21:49:04 -0300

    Se crea router, paginas de signin y signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import React from "react";
-import "./AppDecorator";
+import ForgotPassword from "./views/ForgotPassword";
+import SignIn from "./views/SignIn";
+import SignUp from "./views/SignUp";
 import LandingPage from "./views/LandingPage";
 import { AppDecorator } from "./AppDecorator";
+import { Switch } from "@material-ui/core";
+import { Router, Route } from "react-router";
+import { createBrowserHistory } from "history";
+
+var hist = createBrowserHistory();
 
 export default function App() {
   return AppDecorator({})(function _App() {
     return (
       <React.StrictMode>
-        <LandingPage />
+        <Router history={hist}>
+          <Switch>
+            <Route path="/forgot-password" component={ForgotPassword} />
+            <Route path="/sign-in" component={SignIn} />
+            <Route path="/sign-up" component={SignUp} />
+            <Route path="/" component={LandingPage} />
+          </Switch>
+        </Router>
       </React.StrictMode>
     );
   });
