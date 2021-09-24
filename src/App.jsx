2e2827fd8caf8de3b29482da589fd9b52ac47927
fdealesandro <Francodealesandro@gmail.com>
2021-09-23 23:38:37 -0300

    Se arreglan dependencias

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,26 @@ import ForgotPassword from "./views/ForgotPassword";
 import SignIn from "./views/SignIn";
 import SignUp from "./views/SignUp";
 import LandingPage from "./views/LandingPage";
-import { AppDecorator } from "./AppDecorator";
-import { Switch } from "@material-ui/core";
-import { Router, Route } from "react-router";
-import { createBrowserHistory } from "history";
+import appDecorator from "./AppDecorator";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-var hist = createBrowserHistory();
-
-export default function App() {
-  return AppDecorator({})(function _App() {
-    return (
-      <React.StrictMode>
-        <Router history={hist}>
-          <Switch>
-            <Route path="/forgot-password" component={ForgotPassword} />
-            <Route path="/sign-in" component={SignIn} />
-            <Route path="/sign-up" component={SignUp} />
-            <Route path="/" component={LandingPage} />
-          </Switch>
-        </Router>
-      </React.StrictMode>
-    );
-  });
+function App() {
+  return (
+    <React.Fragment>
+      <Router>
+        <Switch>
+          <Route path="/forgot-password" component={ForgotPassword} />
+          <Route path="/sign-in" component={SignIn} />
+          <Route path="/sign-up" component={SignUp} />
+          <Route path="/" component={LandingPage} />
+        </Switch>
+      </Router>
+    </React.Fragment>
+  );
 }
+
+export default appDecorator(App)
